d3d3f8122c13036ad62ba15606ae4f25f4bb1666
Melanie Munden <melanie.munden@playfish.com>
2012-03-20 13:27:18 +0000

    Fixed Mailbox

--- src/main/scala/dojo/MailBox.scala
+++ src/main/scala/dojo/MailBox.scala
@@ -3,9 +3,15 @@ package dojo
 class MailBox {
 
   def handleMsg(msg: PingPongMessage) = msg match{
+    case Ping => Pong
+    case Pong => Ping
     case _ => None
   }
 
-  def receive(msg: Any) = None
+  def receive(msg: Any) = msg match{
+    case "stop" => "stopped"
+    case x:PingPongMessage  => handleMsg(x)
+    case ("echo", y) => y
+  }
 
 }
